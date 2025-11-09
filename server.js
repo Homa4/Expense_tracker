4e6d217efd1b29986833c961b120247e0ef90604
Homa4 <tn3996580@gmail.com>
2025-11-09 15:11:04 +0200

    Add route imports and middleware setup for user, category, and record management

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import express from "express";
 import "./db/fake_connect.js";
+import userRouter from "./routes/user.js";
+import categoryRouter from "./routes/category.js";
+import recordRouter from "./routes/record.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res("Hello Amigo");
 });
+
+app.use(userRouter);
+app.use(categoryRouter);
+app.use(recordRouter);
 
 app.listen(3000);
